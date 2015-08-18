831432092f4cbad2ab8265f4f89b2f2ce1357bc5
FredHsu <FredHsu0822@hotmail.com>
2015-08-18 15:58:48 +0800

    添加主页面

---
+++ TMCHATTER/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  TMCHATTER
+//
+//  Created by 许文锋 on 15/8/18.
+//  Copyright (c) 2015年 fred. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
