9a22cd82c20910224916c0337669387e519696e2
FredHsu <FredHsu0822@hotmail.com>
2015-08-18 15:50:59 +0800

    Initial Commit

---
+++ TMCHATTER/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TMCHATTER
+//
+//  Created by 许文锋 on 15/8/18.
+//  Copyright (c) 2015年 fred. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
